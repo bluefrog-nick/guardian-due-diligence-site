c45314b13c1ad7853569b916e3fb4def8167a087
Nick Hinzmann <29715424+bluefrog-nick@users.noreply.github.com>
2020-10-21 11:34:13 -0500

    Add files via upload

--- src/templates/showcase.js
+++ src/templates/showcase.js
@@ -40,6 +40,9 @@ export default class Showcase extends React.Component {
                   </div>
                 </div>
               </div>
+              <div className="content-block">
+                <div className="block-item-contents">{_.get(this.props, 'data.showcaseModel.content')}</div>
+              </div>
             </Layout>
         );
     }
